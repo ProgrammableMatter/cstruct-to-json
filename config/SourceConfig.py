650bd5760e30102283102a73ca839949cfcb0866
Raoul Rubien <rubienr@gmail.com>
2016-07-06 18:17:37 +0200

    removed source file from config

--- config/SourceConfig.py
+++ config/SourceConfig.py
@@ -15,6 +15,5 @@ sourceRoots = [
     __homeDir + "/" + __subDir + "/avr/avr-common/utils/uc-core/communication-protocol/CommunicationProtocolTypes.h",
     __homeDir + "/" + __subDir + "/avr/avr-common/utils/uc-core/communication/ManchesterDecodingTypes.h",
     __homeDir + "/" + __subDir + "/avr/avr-common/utils/uc-core/communication/CommunicationTypes.h",
-    __homeDir + "/" + __subDir + "/avr/avr-common/utils/uc-core/communication-protocol/InterpreterTypes.h",
     __homeDir + "/" + __subDir + "/avr/avr-common/utils/uc-core/particle/ParticleStateTypes.h",
 ]
