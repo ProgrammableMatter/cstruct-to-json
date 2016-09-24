6b43bd62e4ead0875b7396cd69839de0b8d41df3
Raoul Rubien <rubienr@gmail.com>
2016-09-25 01:29:11 +0200

    configuration update

--- config/SourceConfig.py
+++ config/SourceConfig.py
@@ -5,6 +5,11 @@ __subDir = "msc/git/particle-firmware/src"
 
 defines = [
     r'-DSIMULATION',
+    # typedef workaround: typedefs are not supported by the underlying pycparser
+    r'-DC_STRUCTS_TO_JSON_PARSER_TYPEDEF_NOT_SUPPORTED_SUPPRESS_REGULAR_TYPEDEFS',
+    # r'-DSampleValueType uint16_t',
+    # r'-DCalculationType float',
+    # r'-DDeviationType float',
 ]
 includes = [
     r'-Iutils/fake_libc_include/',
@@ -15,5 +20,7 @@ sourceRoots = [
     __homeDir + "/" + __subDir + "/avr-common/utils/uc-core/communication-protocol/CommunicationProtocolTypes.h",
     __homeDir + "/" + __subDir + "/avr-common/utils/uc-core/communication/ManchesterDecodingTypes.h",
     __homeDir + "/" + __subDir + "/avr-common/utils/uc-core/communication/CommunicationTypes.h",
+    __homeDir + "/" + __subDir + "/avr-common/utils/uc-core/synchronization/SynchronizationTypes.h",
+    __homeDir + "/" + __subDir + "/avr-common/utils/uc-core/periphery/PeripheryTypes.h",
     __homeDir + "/" + __subDir + "/avr-common/utils/uc-core/particle/ParticleStateTypes.h",
 ]
